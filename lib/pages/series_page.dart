4599ab01745b32426b58de7994c9eba09b478828
megan59781 <megan59781@gmail.com>
2025-05-11 08:25:15 +0100

    ui tweaks

--- lib/pages/series_page.dart
+++ lib/pages/series_page.dart
@@ -11,30 +11,43 @@ class MySeriesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(title),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary),
-      body: SeriesList(
-        pageLink: pageLink,
-        onTap: (pageLink, title, data) {
-          if (pageLink.contains('Filter')) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    FigureFilterPage(title: title, data: data),
-              ),
-            );
-          } else {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Inventory(title: title, data: data),
-              ),
-            );
-          }
-        },
-      ),
-    );
+        appBar: AppBar(
+            title: Text(title),
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary),
+        body: Column(children: [
+          const SizedBox(height: 20.0),
+          const Text(
+            'Choose a series to view the figures',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 15.0),
+          Expanded(
+            child: SeriesList(
+              pageLink: pageLink,
+              onTap: (pageLink, title, data) {
+                if (pageLink.contains('Filter')) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          FigureFilterPage(title: title, data: data),
+                    ),
+                  );
+                } else {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Inventory(title: title, data: data),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ]));
   }
 }
